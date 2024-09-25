7b41f34169fb37b36852b0d9521f349983637867
CarlosMoritcho <20210023297@my.xu.edu.ph>
2024-09-25 14:17:23 +0800

    cleaned main branch

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,18 +4,9 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-     
       fontFamily: {
-        quicksand: ['Quicksand', 'sans-serif'],
-        rubik: ['Rubik', 'sans-serif'],
-      },
-      fontSize: {
-        'h1': ['48px', '1.2'], // 48px size with 1.2 line height
-        'h2': ['32px', '1.2'],
-        'h3': ['24px', '1.2'],
-        'h4': ['16px', '1.2'],
-        'h5': ['14px', '1.2'],
-        'h6': ['12px', '1.2'],
+        Quicksand: ['Quicksand', 'sans-serif'],
+        Rubik: ['Rubik', 'sans-serif'],
       },
     },
   },
